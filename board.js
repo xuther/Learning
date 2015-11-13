f3be56ade40bdebe2e1b297a53b74b719a7e5d6a
Joseph Blodgett <Joseph.Blodgett@gmail.com>
2015-11-13 16:18:34 -0700

    the movements work with the sub div

--- board.js
+++ board.js
@@ -21,18 +21,28 @@ function buildBoard() {
 }
 
 function makeTank(){
-	var tank = document.createElement("div");
-	tank.id = "tank1";
-	tank.className = "tank";
-	tank.curRotation = 0;
-	tank.style.top = 0;
-	tank.style.left = 0;
-	tank.x = 0;
-	tank.y = 0;
-	tank.rotation = "rotate(0deg)"
-	tank.transform = "translate3d(0,0,0)"
-	document.getElementById("board").appendChild(tank);	
+	var tankPar = document.createElement("div");
+	var tankChild = document.createElement("div");
+	
+	
+	tankChild.className = "tank tankChild"
+	tankChild.id = "tank1Child";
+	
 
+	tankPar.className = "tank"
+	tankPar.id = "tank1";
+	tankPar.style.top = "0";
+	tankPar.style.left = "0";
+	
+	tankPar.x = 0;
+	tankPar.y = 0;
+	tankPar.transform = "translate3d(0,0,0)";
+	tankPar.curRotation = 0;
+	tankPar.rotation = "rotate(0deg)";
+	
+	tankPar.appendChild(tankChild);
+	
+	document.getElementById("board").appendChild(tankPar);	
 }
 
 function rotateRight(tank)
@@ -41,13 +51,14 @@ function rotateRight(tank)
 	var rotate = t.curRotation + 90;
 	t.curRotation = rotate;
 	t.rotation  = 'rotate('+ rotate.toString() +'deg)';	
-	applyTransform(tank)
+	applyTransform(tank);
 }
 
 function applyTransform(tank)
 {
 	var t = document.getElementById(tank);
-	t.style.webkitTransform = t.rotation + " " + t.transform;
+	t.style.webkitTransform = t.transform;
+	t.firstChild.style.webkitTransform = t.rotation
 }
 
 function rotateLeft(tank)
@@ -56,7 +67,7 @@ function rotateLeft(tank)
 	var rotate = t.curRotation - 90;
 	t.curRotation = rotate;
 	t.rotation = 'rotate('+ rotate.toString() +'deg)';	
-	applyTransform(tank)
+	applyTransform(tank);
 }
 
 function moveForward(tank)
@@ -66,7 +77,8 @@ function moveForward(tank)
 	direction = direction / 90;
 	
 	if (direction < 0){
-		direction = direction *-1;}
+		direction = 4 + direction;}
+		
 	var x = 0;
 	var y = 0;
 	
@@ -85,13 +97,14 @@ function moveForward(tank)
 			break;
 	}
 	
-	console.log(x.toString() + y.toString())
+	console.log(x.toString() + y.toString());
+	console.log("Direction" + direction.toString());
 	
 	var finalx = t.x + x;
 	var finaly = t.y + y;
-	if (finalx < 0 || finalx > 700)
+	if (finalx < 0 || finalx > 700-35)
 		return;
-	if (finaly < 0 || finaly > 700)
+	if (finaly < 0 || finaly > 700-35)
 		return;
 	t.x = finalx;
 	t.y = finaly;
